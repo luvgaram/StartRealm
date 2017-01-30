d7ff1debe3def91dddddac221bb78d5c7676ca65
Eunjoo <eunjoo.im@realm.io>
2017-01-30 14:49:36 +0900

    Initial Commit

---
+++ MyDiary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyDiary
+//
+//  Created by Eunjoo on 2017. 1. 30..
+//  Copyright © 2017년 Eunjoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
